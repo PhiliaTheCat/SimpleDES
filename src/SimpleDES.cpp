a399e71f295950da7dbfcfe1a5dc2d4f1f63d6c0
PhiliaTheCat <2787974051@qq.com>
2023-12-02 21:58:39 +0800

    Test

--- src/SimpleDES.cpp
+++ src/SimpleDES.cpp
@@ -8,6 +8,7 @@ int main()
     unsigned long long key = 2084160214307458188ll;
     unsigned long long msg = -1410563274867700724ll;
     std::bitset<64> m = msg;
+    std::cout << "message: " << m << std::endl;
     ptc::SubKey k[16];
     ptc::SubKeyGen(key, k);
     std::bitset<64> t = ptc::Init_Perm(m);
@@ -17,6 +18,8 @@ int main()
         ptc::Round(L, R, k[i]);
     t = ptc::Merge(R, L);
     std::bitset<64> res = ptc::Inv_Init_Perm(t);
-    std::cout << res << std::endl;
+    std::cout << "result: " << res << std::endl;
+    for (int i = 0; i < 16; i += 1)
+        std::cout << "key " << i + 1 << ": " << k[i] << std::endl;
     return 0;
 }
